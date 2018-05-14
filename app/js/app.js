5c84ef3e57f23be68201938824c165293ad9f49e
Christopher Miles <mail@christophermiles.co.uk>
2018-05-14 17:26:07 +0100

    Adding weather API call with unit tests

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,9 @@
 var angularJSTestingApp = angular.module('angularJSTestingApp', []);
 
-angularJSTestingApp.controller('MainCtrl', function ($rootScope, $scope) {
+angularJSTestingApp.controller('MainCtrl', function ($rootScope, $scope, $http) {
+    var _this = this;
+    _this.apiKey = "9618a2e646fba37542fa264ddb74f21b";
+
     $scope.title = "Testing AngularJS Applications";
 
     $scope.destinations = [];
@@ -19,5 +22,25 @@ angularJSTestingApp.controller('MainCtrl', function ($rootScope, $scope) {
 
     $scope.removeDestination = function(index) {
         $scope.destinations.splice(index, 1)
+    }
+
+    $scope.getWeather = function (destination) {
+        $http.get("http://api.openweathermap.org/data/2.5/weather?q=" + destination.city + "&appid=" + _this.apiKey).then(
+            function successCallback(response){
+                if(response.data.weather && response.data.weather.length ){
+                    destination.weather = {
+                        main: response.data.weather[0].main,
+                        temp: $scope.convertKelvinToCelcius(response.data.main.temp)
+                    };
+                }
+            },
+            function errorCallback(error){
+                console.log(error);
+            }
+        );
+    }
+
+    $scope.convertKelvinToCelcius = function(temp) {
+        return Math.round(temp - 273);
     }
 });
--- app/js/test/unit/app.spec.js
+++ app/js/test/unit/app.spec.js
@@ -3,15 +3,17 @@ describe('app testing', function(){
     beforeEach(module('angularJSTestingApp'));
 
     describe('MainCtrl', function(){
-        var scope, ctrl;
+        var scope, ctrl, httpBackend;
 
-        beforeEach(inject(function($controller, $rootScope){
+        beforeEach(inject(function($controller, $rootScope, $httpBackend){
             scope = $rootScope.$new();
             ctrl = $controller('MainCtrl', {$scope: scope});
+            httpBackend = $httpBackend;
         }));
 
         afterEach(function(){
-            //Cleanup code
+            httpBackend.verifyNoOutstandingExpectation();
+            httpBackend.verifyNoOutstandingRequest();
         });
 
         it('should initialise the title in the scope', function(){
@@ -24,26 +26,26 @@ describe('app testing', function(){
             expect(scope.destinations.length).toBe(0);
 
             scope.newDestination = {
-                city: "London",
-                country: "England"
+                city: 'London',
+                country: 'England'
             };
 
             scope.addDestination();
 
             expect(scope.destinations.length).toBe(1);
-            expect(scope.destinations[0].city).toBe("London");
-            expect(scope.destinations[0].country).toBe("England");
+            expect(scope.destinations[0].city).toBe('London');
+            expect(scope.destinations[0].country).toBe('England');
 
-            scope.newDestination.city = "Frankfurt";
-            scope.newDestination.country = "Germany";
+            scope.newDestination.city = 'Frankfurt';
+            scope.newDestination.country = 'Germany';
 
             scope.addDestination();
 
             expect(scope.destinations.length).toBe(2);
-            expect(scope.destinations[1].city).toBe("Frankfurt");
-            expect(scope.destinations[1].country).toBe("Germany");
-            expect(scope.destinations[0].city).toBe("London");
-            expect(scope.destinations[0].country).toBe("England");
+            expect(scope.destinations[1].city).toBe('Frankfurt');
+            expect(scope.destinations[1].country).toBe('Germany');
+            expect(scope.destinations[0].city).toBe('London');
+            expect(scope.destinations[0].country).toBe('England');
         });
 
         it('should remove a destination from destinations list', function(){
@@ -60,8 +62,27 @@ describe('app testing', function(){
             scope.removeDestination(0);
 
             expect(scope.destinations.length).toBe(1);
-            expect(scope.destinations[0].city).toBe("Warsaw");
-            expect(scope.destinations[0].country).toBe("Poland");
+            expect(scope.destinations[0].city).toBe('Warsaw');
+            expect(scope.destinations[0].country).toBe('Poland');
+        });
+
+        it('should update the weather for a specific destination', function(){
+            scope.destination = {
+                city: 'Melbourne',
+                country: 'Australia'
+            };
+
+            httpBackend.expectGET('http://api.openweathermap.org/data/2.5/weather?q=' + scope.destination.city + '&appid=' + ctrl.apiKey).respond({
+                weather: [{ main: 'Rain', description: 'light rain' }],
+                main: {temp: 282.85}
+            });
+
+            scope.getWeather(scope.destination);
+
+            httpBackend.flush();
+
+            expect(scope.destination.weather.main).toBe('Rain');
+            expect(scope.destination.weather.temp).toBe(10);
         });
     });
 
