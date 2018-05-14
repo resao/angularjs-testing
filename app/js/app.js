ca7a9ac3f01bbff55f8e098abfbeed03a2d68462
Christopher Miles <mail@christophermiles.co.uk>
2018-05-14 14:38:01 +0100

    Added ability to add and remove destination with accompanying tests

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,23 @@
 var angularJSTestingApp = angular.module('angularJSTestingApp', []);
 
 angularJSTestingApp.controller('MainCtrl', function ($rootScope, $scope) {
-
     $scope.title = "Testing AngularJS Applications";
 
+    $scope.destinations = [];
+
+    $scope.newDestination = {
+        city: undefined,
+        country: undefined
+    };
+
+    $scope.addDestination = function() {
+        $scope.destinations.push({
+            city: $scope.newDestination.city,
+            country: $scope.newDestination.country
+        });
+    };
+
+    $scope.removeDestination = function(index) {
+        $scope.destinations.splice(index, 1)
+    }
 });
--- app/js/test/unit/app.spec.js
+++ app/js/test/unit/app.spec.js
@@ -18,6 +18,51 @@ describe('app testing', function(){
             expect(scope.title).toBeDefined();
             expect(scope.title).toBe('Testing AngularJS Applications');
         });
+
+        it('should add 2 destinations to the destinations list', function(){
+            expect(scope.destinations).toBeDefined();
+            expect(scope.destinations.length).toBe(0);
+
+            scope.newDestination = {
+                city: "London",
+                country: "England"
+            };
+
+            scope.addDestination();
+
+            expect(scope.destinations.length).toBe(1);
+            expect(scope.destinations[0].city).toBe("London");
+            expect(scope.destinations[0].country).toBe("England");
+
+            scope.newDestination.city = "Frankfurt";
+            scope.newDestination.country = "Germany";
+
+            scope.addDestination();
+
+            expect(scope.destinations.length).toBe(2);
+            expect(scope.destinations[1].city).toBe("Frankfurt");
+            expect(scope.destinations[1].country).toBe("Germany");
+            expect(scope.destinations[0].city).toBe("London");
+            expect(scope.destinations[0].country).toBe("England");
+        });
+
+        it('should remove a destination from destinations list', function(){
+            scope.destinations = [{
+                city: 'Paris',
+                country: 'France'
+            },{
+                city: 'Warsaw',
+                country: 'Poland'
+            }];
+
+            expect(scope.destinations.length).toBe(2);
+
+            scope.removeDestination(0);
+
+            expect(scope.destinations.length).toBe(1);
+            expect(scope.destinations[0].city).toBe("Warsaw");
+            expect(scope.destinations[0].country).toBe("Poland");
+        });
     });
 
 });
